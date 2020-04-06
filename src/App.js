9fa94344862d799b247708724389328a45d2be76
Nick Oelsner <noelsner@gmail.com>
2020-04-05 21:47:51 -0700

    searchbar update and start of displaying jobs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
 import React, {useEffect, useState} from 'react';
 import { Route } from 'react-router-dom';
 import fetchJobs from './api.js';
-import Jobs from './Jobs.js';
 import Navbar from './navbar';
 import SearchBar from './SearchBar';
+import Jobs from './jobs';
+import Sidebar from './jobs/Sidebar.js';
+import Details from './jobs/Details'
 import SavedJobs from './SavedJobs.js';
 
+const fakeAPIResponse =
+  {  
+    "version":2,
+    "query":"java",
+    "location":"austin, tx",
+    "dupefilter":true,
+    "highlight":false,
+    "radius":25,
+    "start":1,
+    "end":10,
+    "totalResults":547,
+    "pageNumber":0,
+    "results":[  
+      {  
+        "jobtitle":"Java Developer",
+        "company":"XYZ Corp.,",
+        "city":"Austin",
+        "state":"TX",
+        "country":"US",
+        "formattedLocation":"Austin, TX",
+        "source":"Dice",
+        "date":"Mon, 02 Aug 2017 16:21:00 GMT",
+        "snippet":"looking for an object-oriented Java Developer... Java Servlets, HTML, JavaScript, AJAX, Struts, desirable. Familiarity with Tomcat and the Java...",
+        "url":"https://www.indeed.com/viewjob?jk=12345&indpubnum=8343699265155203",
+        "onmousedown":"indeed_clk(this, '0000');",
+        "latitude":30.27127,
+        "longitude":-97.74103,
+        "jobkey":"12345",
+        "sponsored":false,
+        "expired":false,
+        "indeedApply":true,
+        "formattedLocationFull":"Austin, TX",
+        "formattedRelativeTime":"11 hours ago"
+      }
+    ]
+  }
+
 const App = ()=> {
-  const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState([fakeAPIResponse]);
+  const [savedJobs, setSavedJobs] = useState([]);
 
   useEffect(() => {
     console.log("In useEffect");
-    fetchJobs().then((jobs) => setJobs(jobs));
+    fetchJobs().then((jobs) => setSavedJobs(jobs));
   }, []);
 
   console.log("In job_listings App")
@@ -20,7 +60,17 @@ const App = ()=> {
       <Navbar />
       <Route path='/' exact>
         <SearchBar />
-        <Jobs jobs={jobs}/>
+        <div className='flex flex-row w-full h-screen px-6 mt-12'>
+          <div className='w-1/5'>
+            <Sidebar jobs={jobs} />
+          </div>
+          <div className='w-2/5'>
+            <Jobs jobs={jobs} />
+          </div>
+          <div className='w-2/5'>
+            <Details jobs={jobs} />
+          </div>
+        </div>
       </Route>
       <Route path='jobs/saved' exact>
         <SavedJobs />
