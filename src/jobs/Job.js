09c7d4e75ce2d6bdb737c8c10bc550c6b8c25bc5
Nick Oelsner <noelsner@gmail.com>
2020-04-07 22:37:20 -0700

    updated save job button, broke job into component

---
+++ src/jobs/Job.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import Heart from '../icons/heart';
+
+const Job = ({job}) => {
+  const [savedBtn, setSavedBtn] = useState(false)      
+
+  const toggleSaved = (ev, id) => {
+    ev.preventDefault();
+    setSavedBtn(!savedBtn);
+    saveJob(id);
+  };
+  
+  const saveJob = (id) => console.log(`Job with id=${id} saved!!!`); 
+  
+  return(
+    <li key={job.id}>
+      <div className='bg-gray-800 text-gray-600 mt-6 rounded-lg p-4'>
+        <div className='flex justify-between align-middle w-full'>
+          <a className='text-gray-300 text-2xl mr-10 leading-tight'>{job.title}</a>
+          <button onClick={ev => toggleSaved(ev, job.id)} className='focus:outline-none focus:text-gray-500'>
+            <Heart classes={`fill-current text-gray-${savedBtn ? '200' : '700'} w-4 h-4`} />
+          </button>
+        </div>
+        <div className='text-gray-400 text-lg font-bold mt-2'>{job.company}</div>
+        <div className='text-gray-400 text-lg mt-2 leading-snug'>{job.location}</div>
+        <div className='text-gray-500 text-md mt-3'>
+          <span className='capitalize leading-snug'>{job.description.slice(0,100)}</span>
+          <span>{/*results.snippet.slice(1, results.snippet.length)*/}</span>
+        </div>
+        <button onClick={ev => {
+          toggleSaved(ev, job.id)
+          // saveJob(job.id)
+        }} className='block text-gray-400 text-xs underline mt-2'>Save Job</button>
+      </div>
+    </li>
+  )
+}
+
+export default Job;
