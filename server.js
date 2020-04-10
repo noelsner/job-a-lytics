ac21bcc54abfdc51b71c5fa13f432191fdb90c35
Philip Sun <philsun@gmail.com>
2020-04-09 21:18:54 -0700

    Revert "Single query"

--- server.js
+++ server.js
@@ -15,20 +15,24 @@ app.get('/', (req, res, next)=> {
 
 });
 
-app.get('/api/github', (req, res, next) => { 
+app.get('/api/github/description=:inputQuery&location=:inputLocation', (req, res, next) => { 
+  
   console.log("***** server.js Initiating Axios call to GitHub *****");
   console.log("Recieved parameters: ");
-  console.log(req.query);
-  const {description, location} = req.query;
-  const url = `https://jobs.github.com/positions.json?description=${description}&location=${location}`;
+  console.log(req.params);
+  const { inputQuery, inputLocation } = req.params; 
+  console.log(inputQuery);
+  console.log(inputLocation);
+  
+  const url = `https://jobs.github.com/positions.json?description=${inputQuery}&location=${inputLocation}`;
   axios.get(url)
     .then( response => {
-      res.send(response);
-      })
+      res.send(response.data);
+    })
     .catch( ex => {
       res.send(ex);
       next;
-      })
+  })
   
 })
 
