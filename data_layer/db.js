419689fdab908e084572603f8adc0337fdcf4646
SEJohnson62 <56454735+SEJohnson62@users.noreply.github.com>
2020-04-09 22:16:49 -0700

    Update db.js
    bug fix

--- data_layer/db.js
+++ data_layer/db.js
@@ -1,15 +1,15 @@
 const { client } = require("./client");
 const { createListing } = require("./index");
 const { createUser } = require("./users");
+const { createFavorite } = require("./favorites");
 
 const sync = async() => {
 
   const SQL = `
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-    DROP TABLE IF EXISTS favorite_listings;
+    DROP TABLE IF EXISTS favorites;
     DROP TABLE IF EXISTS job_listings;
     DROP TABLE IF EXISTS users;
-    DROP TABLE IF EXISTS roles;
 
     CREATE TABLE users(
       id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -19,7 +19,7 @@ const sync = async() => {
       password VARCHAR(100),
       CHECK (char_length(username) > 0),
       CHECK (char_length("firstName") > 0),
-      role VARCHAR(20) NOT NULL,
+      role VARCHAR(20)
     );
 
     CREATE TABLE job_listings(
@@ -29,7 +29,7 @@ const sync = async() => {
       listing_url VARCHAR(100),
       job_type VARCHAR(20) DEFAULT 'FULL-TIME',
       company_name VARCHAR(50) NOT NULL,
-      CHECK (length(company_name) > 0),q
+      CHECK (length(company_name) > 0),
       location VARCHAR(50) NOT NULL,
       job_title VARCHAR(50) NOT NULL,
       contact VARCHAR(100) NOT NULL,
@@ -38,16 +38,16 @@ const sync = async() => {
       job_description TEXT
     );
 
-    CREATE TABLE favorite_listings(
+    CREATE TABLE favorites(
       id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
       date TIMESTAMP default CURRENT_TIMESTAMP,
       listing_id UUID REFERENCES job_listings(id),
       user_id UUID REFERENCES users(id)
     );
     `
-
+console.log("calling client.query")
     await client.query(SQL);
-
+console.log("returned from client.query")
     // Seed data
 
     //Job types are FULL-TIME, PART-TIME, CONTRACT, TEMPORARY, INTERNSHIP
