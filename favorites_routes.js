e7073edfc0c2f9a94bb32ec7eabc7232b1024ec2
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-04-11 11:57:11 -0700

    added favorite-router

--- favorites_routes.js
+++ favorites_routes.js
@@ -1,24 +1,13 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
 const path = require('path');
 const { createFavorite, readFavorites, updateFavorite, deleteFavorite } = require("./data_layer/index.js");
 
-// body parser
-app.use(express.json());
-
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
-
-app.get('/', (req, res, next)=> {
-  res.sendFile(path.join(__dirname, 'index.html'));
-
-});
-
 // Database favorites Create Route
 
-app.post('/api/favorites', async(req, res, next)=> {
+router.post('', async(req, res, next)=> {
   const company_name = req.body;
-  console.log("In favorites_routes.js, app.post")
+  console.log("In favorites_routes.js, router.post")
   console.log(req.body);
   console.log(req.params);
   createFavorite(listing_id, user_id)
@@ -28,36 +17,43 @@ app.post('/api/favorites', async(req, res, next)=> {
 
 // Database favorites Read Route
 // TODO: pass user id into readFavorites
-app.get('/api/favorites', async(req, res, next)=> {
+router.get('', async(req, res, next)=> {
+  res.send("You've made it to favorites");
+  
   readFavorites()
   .then( response => res.send(response) )
   .catch( next )
+  
 });
 
 // Database favorites Update Route
-app.put('/api/favorites/:id', async(req, res, next)=> {
+router.put('/:id', async(req, res, next)=> {
   updateFavorite({...req.body, id: req.params.id})
   .then( response => res.send(response) )
   .catch( next )
 });
 
 // Database favorites Delete Route
-app.delete('/api/favorites/:id', async(req, res, next)=> {
+router.delete('/:id', async(req, res, next)=> {
   deleteFavorite( req.params.id )
   .then( response => res.send(response) )
   .catch( next )
 });
 
 // Error handlers
-app.use((req, res, next)=> {
+router.use((req, res, next)=> {
   next({
     status: 404,
     message: `Page not found for ${req.method} ${req.url}`
   })
 });
 
-app.use((err, req, next)=> {
+router.use((err, req, next)=> {
   res.status(err.status || 500).send({
     message: err.message || JSON.stringify(err)
   });
 });
+
+module.exports = {
+  router
+}
