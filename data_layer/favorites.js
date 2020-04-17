906bbb635bcd81e2459a52483dd43cc3da159a02
Susan E. Johnson <seagull62@icloud.com>
2020-04-16 20:17:53 -0700

    initial data_layer update and delete functions

--- data_layer/favorites.js
+++ data_layer/favorites.js
@@ -8,23 +8,27 @@ const createFavorite = async({ listing_id, user_id })=> {
 };
 
 const readFavorites = async( user_id ) => {
-  console.log("In readFavorites");
-  const SQL = 'SELECT * from favorites';
-  const response = await client.query(SQL,[]);
+  console.log("In readFavorites, user_id = ", user_id);
+  const SQL = 'SELECT * from favorites WHERE user_id = $1';
+  const response = await client.query(SQL,[user_id]);
   //console.log(response.rows);
   return (response.rows);
 };
-/*
-console.log("In readFavorites, user_id = ", user_id);
-return( await client.query('SELECT * from favorites WHERE user_id = $1'),[user_id]).rows;
-*/
 
-const updateFavorite = async( user_id )=> {
-  console.log("In updateFavorite");
+const updateFavorite = async( favorite )=> {
+  console.log("In updateFavorite, id = ", favorite.id);
+
+  const SQL = 'UPDATE favorites SET listing_id = $1, user_id = $2 WHERE id = $3 returning *';
+
+  const response = await client.query(SQL, [favorite.listing_id, favorite.user_id, favorite.id]);
+
+  return response.rows[0];
 };
 
-const deleteFavorite = async( user_id )=> {
-  console.log("In deleteFavorite");
+const deleteFavorite = async( id )=> {
+  console.log("In deleteFavorite, id = ", id);
+  const SQL = 'DELETE FROM favorites WHERE id = $1';
+  return (await client.query(SQL, [id])).rows;
 };
 
 const checkForFavorites = async (id) => {
