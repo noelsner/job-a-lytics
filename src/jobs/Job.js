f23121899b0e0bec210c0c9aa984e073b665eab7
Nick Oelsner <noelsner@gmail.com>
2020-04-16 19:34:17 -0700

    updated per reorg

--- src/jobs/Job.js
+++ src/jobs/Job.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import Heart from '../icons/heart';
-import axios from "axios";
+import { Link } from 'react-router-dom';
 
 const Job = ({job, savedJobs}) => {
   const [savedBtn, setSavedBtn] = useState(false)
@@ -16,24 +16,21 @@ const Job = ({job, savedJobs}) => {
     savedJobs.push(job);
 
     //TODO: add this job to the listings table (& favorites table for the current user)
-    /*axios.post(`/api/job_listings`,job)
-    .then(response => console.log(response))
-    .catch()*/
   }
 
   return(
     <li key={job.id}>
       <div className='bg-gray-800 text-gray-600 mt-6 rounded-lg p-4'>
         <div className='flex justify-between align-middle w-full'>
-          <a className='text-gray-300 text-2xl mr-10 leading-tight'>{job.job_title}</a>
+          <Link to={`/jobs/${job.id}`} className='text-gray-300 text-2xl mr-10 leading-tight'>{job.title}</Link>
           <button onClick={ev => toggleSaved(ev, job.id)} className='focus:outline-none focus:text-gray-500'>
             <Heart classes={`fill-current text-gray-${savedBtn ? '200' : '700'} w-4 h-4`} />
           </button>
         </div>
-        <div className='text-gray-400 text-lg font-bold mt-2'>{job.company_name}</div>
+        <div className='text-gray-400 text-lg font-bold mt-2'>{job.company}</div>
         <div className='text-gray-400 text-lg mt-2 leading-snug'>{job.location}</div>
         <div className='text-gray-500 text-md mt-3'>
-          <span className='capitalize leading-snug'>{job.job_description.slice(0,100)}</span>
+          <span className='capitalize leading-snug'>{job.description.slice(0,100)}</span>
           <span>{/*results.snippet.slice(1, results.snippet.length)*/}</span>
         </div>
         <button onClick={ev => {
---
+++ src/jobs/JobResults.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Job from './Job';
+
+const JobResults = ({ jobs, savedJobs, setSavedJobs }) => {
+  return (
+    <div className='px-3'>
+      <header>
+        <div className='text-gray-500 text-xl'>Results for "{jobs[0].query}" in {jobs[0].location}.</div>
+        <div className='text-gray-600 text-sm'>Showing 1-10 of {jobs[0].totalResults} jobs.</div>
+      </header>
+      <ul>
+        {
+          jobs.map((job) => {
+            return (
+              <Job job={job} key={job.id} savedJobs={savedJobs} setSavedJobs={setSavedJobs}/>
+            )
+          })
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default JobResults;
