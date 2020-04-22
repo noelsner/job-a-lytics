af9b719c4435d675650374559a2ba15521c33183
Susan E. Johnson <seagull62@icloud.com>
2020-04-22 12:43:36 -0700

    seach results header enhancements

--- src/jobs/JobResults.js
+++ src/jobs/JobResults.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Job from './Job';
 
-const JobResults = ({ jobs, savedJobs, favorites, addToFavorites, removeFromFavorites, auth, setTempJob }) => {
+const JobResults = ({ jobs, savedJobs, favorites, addToFavorites, removeFromFavorites, auth, setTempJob, inputQuery, inputLocation }) => {
+
   return (
     <div className='pl-3 pr-3 md:pr-0'>
       <header>
-        <div className='text-gray-500 text-xl'>Results for "{jobs[0].query}" in {jobs[0].location}.</div>
-        <div className='text-gray-600 text-sm'>Showing 1-10 of {jobs[0].totalResults} jobs.</div>
+        <div className='text-gray-500 text-xl'>Results for "{inputQuery}" in "{inputLocation}":</div>
+        <div className='text-gray-600 text-sm'>Showing {jobs.length} jobs.</div>
       </header>
       <ul>
         {
--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import JobResults from './JobResults';
 import Sidebar from './Sidebar';
 
 const Jobs = ({jobs, setJobs, savedJobs, favorites, addToFavorites, removeFromFavorites, auth, setTempJob}) => {
-  return (
+
+  // These state variables are set by user input in SearchBar
+  const [inputQuery, setInputQuery] = useState(' ');
+  const [inputLocation, setInputLocation] = useState(' ');
+
+ return (
     <Switch>
       <Route>
-        <SearchBar setJobs = {setJobs}/>
+        <SearchBar setJobs = {setJobs}
+              inputQuery = {inputQuery} setInputQuery={setInputQuery}
+              inputLocation={inputLocation} setInputLocation = {setInputLocation}/>
         <div className='flex flex-col md:flex-row w-full h-screen px-6 mt-8 md:mt-12'>
           <div className='md:w-32 px-3 md:pl-0 mb-6 md:mb-0'>
             <Sidebar jobs={jobs} setJobs={setJobs}/>
           </div>
           <div className='w-full'>
-            <JobResults jobs={jobs} savedJobs={savedJobs} favorites={favorites} addToFavorites={addToFavorites} removeFromFavorites={removeFromFavorites} auth={auth} setTempJob={setTempJob} />
+            <JobResults jobs={jobs} savedJobs={savedJobs}
+                favorites={favorites} addToFavorites={addToFavorites} removeFromFavorites={removeFromFavorites}
+                auth={auth} setTempJob={setTempJob}
+                inputQuery={inputQuery} setInputQuery={setInputQuery} inputLocation={inputLocation} />
           </div>
         </div>
       </Route>
-      {/* <Route path='/job/:id' >
-        <Details jobs={jobs} />
-      </Route> */}
     </Switch>
   );
 };
