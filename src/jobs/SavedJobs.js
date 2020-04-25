d3274bb45a6e8bdc317061c5f1ea197a45e77976
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-04-25 12:12:35 -0700

    initial savedJobs graph implemented

--- src/jobs/SavedJobs.js
+++ src/jobs/SavedJobs.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import Job from './Job';
+import SavedJobsStats from './SavedJobsStats';
 
 const SavedJobs = ({ savedJobs, favorites, addToFavorites, removeFromFavorites, auth, setTempJob, savedJobSet }) => {
   console.log('savedJobs :>> ', savedJobs);
   return (
     <div className='px-3'>
+      
       { savedJobs.length ? (
         <div>
           <header>
             <div className='text-gray-300 text-xl mt-6'>Your Saved Jobs ({savedJobs.length})</div>
           </header>
-          <ul>
-            {
-              savedJobs.map((job) => {
-                return (
-                  <Job job={job} key={job.listingId} savedJobs={savedJobs} favorites={favorites} addToFavorites={addToFavorites} removeFromFavorites={removeFromFavorites} auth={auth} setTempJob={setTempJob} savedJobSet={savedJobSet} />
-                )
-              })
-            }
-          </ul>
+          <div className='flex flex-row flex-wrap justify-between'>
+            <ul>
+              {
+                savedJobs.map((job) => {
+                  return (
+                    <Job job={job} key={job.listingId} savedJobs={savedJobs} favorites={favorites} addToFavorites={addToFavorites} removeFromFavorites={removeFromFavorites} auth={auth} setTempJob={setTempJob} savedJobSet={savedJobSet} />
+                  )
+                })
+              }
+            </ul>
+            <SavedJobsStats savedJobs = {savedJobs}/>
+          </div>
         </div>
       ) : (
         <div>
