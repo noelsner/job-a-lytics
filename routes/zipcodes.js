4550a5c5eb1ab52327451eccb161db3c3414faf5
Susan E. Johnson <seagull62@icloud.com>
2020-04-28 22:23:04 -0700

    getZipsFromCity

--- routes/zipcodes.js
+++ routes/zipcodes.js
@@ -1,15 +1,29 @@
 const axios = require('axios');
 const express = require('express');
 const router = express.Router();
+APIkey = 'CCfGLRhnGaQZ4YqoTFOIX8A8ocJJueJa4CCydPNskRstjsyjD07mAwBNeTm5PiUA'
 
 const getZipsWithinRadius = async ( centerZip, distance ) => {
   try {
-    const URL = `https://www.zipcodeapi.com/rest/CCfGLRhnGaQZ4YqoTFOIX8A8ocJJueJa4CCydPNskRstjsyjD07mAwBNeTm5PiUA/radius.json/${centerZip}/${distance}/mile`;
+    const URL = `https://www.zipcodeapi.com/rest/${APIkey}/radius.json/${centerZip}/${distance}/mile`;
 
     console.log(URL);
     const response = await axios.get(URL);
     const zipArray = response.data;
     console.log("zipcodes within ", distance," are:",zipArray);
+    return zipArray;
+  } catch (error) {
+    console.error( "in getZips",error.message );
+  }
+}
+
+const getZipsFromCity = async ( city, state ) => {
+  try {
+    const URL = `https://www.zipcodeapi.com/rest/${APIkey}/city-zips.json/${city}/${state}`
+    console.log(URL);
+    const response = await axios.get(URL);
+    const zipArray = response.data;
+    console.log("zipcodes within ", city," are:",zipArray);
     return zipArray;
   } catch (error) {
     console.error( "in getZips",error.message );
